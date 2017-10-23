2e526e43d228b616d4f1de690adf5a27bd7ad6ff
Naxiden <bialikandrzej10@gmail.com>
2017-10-23 18:43:36 +0200

    handle email, password ref and added promise not done yet

--- firebase-login/src/Authen.js
+++ firebase-login/src/Authen.js
@@ -11,12 +11,45 @@ var config = {
   firebase.initializeApp(config);
 
 class Authen extends Component {
+
+login(event) {
+//TODO: HANDLE PROMISE LOG IN
+const email = this.refs.email.value;
+const password = this.refs.password.value;
+console.log(email, password)
+
+const auth = firebase.auth()
+const promise = auth.signInWithEmailAndPassword(email, password);
+promise.catch(e => {
+  var err = e.message;
+console.log(err);
+this.setState({err:err});
+
+})
+
+}
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      err:''
+    };
+    this.login = this.login.bind(this);
+  }
+
+
+
   render(){
     return(
       <div>
         <h2>Am from Authen dang yo</h2>
         <input id="email" ref="email" type="email" placeholder="Enter your email" /><br />
         <input id="pass" ref="password" type="password" placeholder="Enter your password" /><br />
+        <p>{this.state.err}</p>
+        <button onClick={this.login}>Log in</button>
+        <button>Sing up</button>
+        <button>Log out</button>
       </div>
     );
   }
